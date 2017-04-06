7e5ad92146028c6bbc4814aa7972214d40eb4b47
Jyothicrb <jyothicrb@gmail.com>
2017-04-06 15:20:41 +0530

    Create WassUp.js

---
+++ WassUp.js
@@ -0,0 +1,24 @@
+<!-- First react script -->
+
+function fname(user) 
+{
+  return user.firstName + "  " + user.secondName ;
+};
+
+const user = {
+  firstName: "Jyo", 
+  secondName: "Pa"
+};
+const someName = (
+  <h1> what's Up !! {fname(user)} </h1>);
+    
+ReactDOM.render(
+    someName,
+      document.getElementById('root')
+    )
+    
+    <!-- 
+    *<div id="root">
+    *<!-- This element's contents will be replaced with your component. -->
+    *</div>
+    -->
