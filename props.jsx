57f5e8dd0d594bd1143a83192a228091729f800e
Jyothicrb <jyothicrb@gmail.com>
2017-04-06 16:53:39 +0530

    Create props.jsx

---
+++ props.jsx
@@ -0,0 +1,8 @@
+function Welcome(props) {
+  return <h1> Hello {props.name} </h1>;  
+}
+const element = <Welcome name ="Jyothi"/>;
+ReactDOM.render( element,
+                document.getElementById('root')
+
+); 
